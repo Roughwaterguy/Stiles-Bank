dccd030bad16e2a0c498fbb4499fbf705cad10b3
Roughwaterguy <107322585+Roughwaterguy@users.noreply.github.com>
2023-10-06 12:57:41 -0400

    dsdfsd

--- submit.php
+++ submit.php
@@ -1,11 +1,11 @@
 <?php
   $name = $_POST['name'];
   $email = $_POST['email'];
-  $email = $_POST['oth'];
+  $oth = $_POST['oth'];
   
   $to = "s27.jcaldwell@sels.us";
-  $subject = "New form submission";
-  $body = "Name: " . $name . "\nEmail: " . $email;
+  $subject = "Form emails";
+  $body = $name . $email . $oth;
   
   if (mail($to, $subject, $body)) {
     echo "There was an issue Please try again";
